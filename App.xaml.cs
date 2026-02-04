e59a0aa47a72e0f6e0711636fc9467f5b1a79277
Nyu <Nyu@IVY>
2026-02-04 18:33:50 +0100

    Ajoutez des fichiers projet.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace N.I.C.E.___Nextspace_Intelligent_Combo_Evaluator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
